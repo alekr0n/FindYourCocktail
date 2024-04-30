410ec7171885ac59030791a539203e6c06de3c55
alekr0n <121205877+alekr0n@users.noreply.github.com>
2024-04-30 18:54:10 +0200

    third part of local solution

---
+++ Cocktails.Application/Interfaces/ICocktailsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cocktails.Domain;
+
+namespace Cocktails.Application.Interfaces
+{
+    public interface ICocktailsDbContext
+    {
+        DbSet<Cocktail> Cocktails { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
